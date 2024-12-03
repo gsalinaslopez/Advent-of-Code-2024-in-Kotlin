dc07d59055133a244b556fe5c1207ba44b45640f
Giovanni Salinas <gsalinaslopez.cs01@nctu.edu.tw>
2024-12-03 21:38:02 +0800

    Day 1, part 1 and part 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,20 +1,38 @@
+import kotlin.math.abs
+
+fun getColumns(lines: List<String>): Pair<List<Int>, List<Int>> =
+    lines.map {
+        it.split(" +".toRegex()).run {
+            Pair(first().toInt(), last().toInt())
+        }
+    }.unzip()
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val columns = getColumns(input)
+        val leftColumn = columns.first.sorted()
+        val rightColumn = columns.second.sorted()
+
+        return leftColumn.zip(rightColumn) { l, r -> abs(l - r) }.sum()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val columns = getColumns(input)
+        val leftColumn = columns.first
+        val rightColumn = columns.second
+
+        val rightColumnCount = hashMapOf<Int, Int>()
+        rightColumn.forEach { entry ->
+            rightColumnCount[entry] = rightColumnCount.getOrDefault(entry, 0) + 1
+        }
+
+        return leftColumn.map { rightColumnCount.getOrDefault(it, 0) * it }.sum()
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
